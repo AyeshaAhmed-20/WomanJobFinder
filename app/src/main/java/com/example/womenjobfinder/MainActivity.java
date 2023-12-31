929d72ba1a3962fb0bd1ba04089b6be3d126c3ac
Abdullah Najam <laibanasir434@gmail.com>
2023-12-31 16:53:48 +0500

    auth

---
+++ app/src/main/java/com/example/womenjobfinder/MainActivity.java
@@ -0,0 +1,89 @@
+package com.example.womenjobfinder;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+public class MainActivity extends AppCompatActivity {
+
+    private EditText editTextEmail, editTextPassword;
+    private Button buttonLogin;
+
+    private FirebaseAuth mAuth;
+
+    TextView signUpText;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        mAuth = FirebaseAuth.getInstance();
+
+        editTextEmail = findViewById(R.id.editTextEmail);
+        editTextPassword = findViewById(R.id.editTextPassword);
+        buttonLogin = findViewById(R.id.buttonLogin);
+        signUpText = findViewById(R.id.textViewSignUp);
+
+        signUpText.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, SignUpActivity.class);
+                startActivity(intent);
+            }
+        });
+        buttonLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                loginUser();
+            }
+        });
+    }
+
+    private void loginUser() {
+        String email = editTextEmail.getText().toString();
+        String password = editTextPassword.getText().toString();
+
+        mAuth.signInWithEmailAndPassword(email,password).addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task) {
+                if (task.isSuccessful()) {
+                    // Sign in success, update UI with the signed-in user's information
+                    FirebaseUser user = mAuth.getCurrentUser();
+                    Toast.makeText(MainActivity.this, "Authentication successful.",
+                            Toast.LENGTH_SHORT).show();
+                    // Add code to navigate to the next activity or perform other actions
+                } else {
+                    // If sign in fails, display a message to the user.
+                    Toast.makeText(MainActivity.this, "Authentication failed.",
+                            Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+
+
+
+
+
+
+    }
+
+    public void goToSignUpScreen() {
+        // Navigate to the SignUpActivity
+        Intent intent = new Intent(MainActivity.this, SignUpActivity.class);
+        startActivity(intent);
+    }
+}
---
+++ app/src/main/java/com/example/womenjobfinder/SignUpActivity.java
@@ -0,0 +1,64 @@
+package com.example.womenjobfinder;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+
+public class SignUpActivity extends AppCompatActivity {
+
+    private EditText editTextEmailSignUp, editTextPasswordSignUp;
+    private Button buttonSignUp;
+
+    private FirebaseAuth mAuth;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sign_up);
+
+        mAuth = FirebaseAuth.getInstance();
+
+        editTextEmailSignUp = findViewById(R.id.editTextEmailSignUp);
+        editTextPasswordSignUp = findViewById(R.id.editTextPasswordSignUp);
+        buttonSignUp = findViewById(R.id.buttonSignUp);
+
+        buttonSignUp.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                signUpUser();
+            }
+        });
+    }
+
+    private void signUpUser() {
+        String email = editTextEmailSignUp.getText().toString();
+        String password = editTextPasswordSignUp.getText().toString();
+
+        mAuth.createUserWithEmailAndPassword(email,password).addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task) {
+                if (task.isSuccessful()) {
+                    // Sign up success, update UI with the signed-in user's information
+                    Toast.makeText(SignUpActivity.this, "Sign up successful.",
+                            Toast.LENGTH_SHORT).show();
+                    // Add code to navigate to the next activity or perform other actions
+                } else {
+                    // If sign up fails, display a message to the user.
+                    Toast.makeText(SignUpActivity.this, "Sign up failed.",
+                            Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+
+
+    }
+}
